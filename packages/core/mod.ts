7b5f8fdf89c2c0595bc7ec14bfe32a34498ef7ab
parksb <parkgds@gmail.com>
2026-02-17 17:27:07 +0900

    feat: make some apis public

--- packages/core/mod.ts
+++ packages/core/mod.ts
@@ -6,4 +6,6 @@ export type {
   DocumentDict,
   Reference,
 } from "./src/document.ts";
-export { type Config, defineConfig, type Hooks } from "./src/config.ts";
+export { type Config, DEFAULT_CONFIG, defineConfig, type Hooks } from "./src/config.ts";
+export { getLinkRegex, type LinkStyle, resolveLink } from "./src/link.ts";
+export { findSubdocs } from "./src/markdown.ts";
